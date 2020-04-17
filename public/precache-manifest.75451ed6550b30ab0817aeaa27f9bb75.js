c58ff93852457d893551c0100bd76de73d85e0e9
Lucas Dramowicz <39057524+ldramowicz@users.noreply.github.com>
2020-04-17 15:24:50 -0400

    Add files via upload

---
+++ public/precache-manifest.75451ed6550b30ab0817aeaa27f9bb75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4964ad3690cee29f783b7b4a1558fd85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1adda17cb4978af50f1",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d8090289222c64255025",
+    "url": "/static/css/main.718047e1.chunk.css"
+  },
+  {
+    "revision": "a1adda17cb4978af50f1",
+    "url": "/static/js/2.657880b7.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.657880b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8090289222c64255025",
+    "url": "/static/js/main.7f509e0b.chunk.js"
+  },
+  {
+    "revision": "0a1291182c642c042143",
+    "url": "/static/js/runtime-main.9dc6353f.js"
+  }
+]);
